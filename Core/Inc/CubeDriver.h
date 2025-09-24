e52907802911d51cc1e2f2e7b196d92802322862
3axap2509 <3axap2509@gmail.com>
2025-09-24 04:04:49 +0300

    added required files to VCS

---
+++ Core/Inc/CubeDriver.h
@@ -0,0 +1,15 @@
+enum DriverCommands
+{
+    ResetAll = 11,
+    ResetLayer = 12,
+    ResetLayers = 13,
+
+    SetDelay = 21,
+
+    DrawLine = 31,
+    DrawDot = 32,
+    DrawSquare = 33,
+    DrawCircle = 34,
+
+    AddLayer = 41,
+};
